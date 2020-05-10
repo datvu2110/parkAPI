d0dfe50d2df80185a4a85c2ab71c07565df7d670
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2020-05-10 17:01:40 -0500

    Final Product

---
+++ script.js
@@ -0,0 +1,65 @@
+function queryArray(formatArr,maxRes){
+	let search ='';
+	for (let i =0; i<formatArr.length; i++){
+		search+= "stateCode=" +formatArr[i] + "&"
+	}
+	search+= "limit=" + maxRes;
+	return search;
+}
+
+
+function handleSubmit(){
+	$("#submit-states").on('submit', function(event) {
+		event.preventDefault();
+		$(".list").empty();
+		$(".error").empty();
+		getParks();
+	});
+}
+
+function getParks(){
+	let maxRes = $('.enter-results').val();
+	let stateArr = $('.enter-states').val();
+	let formatArr = stateArr.split(",");
+	formatArr = formatArr.map(function (el) {
+  		return el.trim().toUpperCase();
+	});
+	const key = 'QdGRZjm3TDc4W4RZb8T7YGJEEe13792EtlXiDgWU';
+	const baseURI = 'https://api.nps.gov/api/v1/parks?';
+	const search  = queryArray(formatArr, maxRes);
+	let URL = baseURI + search+ "&api_key=" +'&fields=addresses'+ key;
+	console.log(URL);
+	fetch(URL)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('.error').text(`Something went wrong...`);
+        });
+}
+
+function displayResults(responseJson){
+	console.log(responseJson)
+	if (responseJson.total <=0){
+		$('.list').append(`<h2>Check your search again</h2>`);
+	}
+	else{
+		$('.list').append(`<h2 class='title'>Here is the results:</h2>`);
+		for (let i = 0; i < responseJson.data.length & i < responseJson.limit; i++) {
+        	$('.list').append(`<li class="title"><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+        	<p class="description">${responseJson.data[i].description}</p>
+        	</li>`);
+    	}
+	}
+	
+}
+
+
+$(function() {
+  console.log('App loaded!');
+  handleSubmit();
+});
